3a5e86d44ece24e8039291ff7e3dd682c556f246
Sam <samuelhalloum@gmail.com>
2023-04-07 16:50:08 -0500

    created a function to prompt user and saved input as an array

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ var lowercase = range(97,123)
 var uppercase = range(65,91)
 var numbers = range(48, 58)
 var special = range(33,48)
+var userInput = []
 
 function range(start, stop){
   // produces a range between two values exclusive of the stop value
@@ -25,6 +26,18 @@ function range(start, stop){
     r.push(i)
   }
   return r
+}
+
+function promptUser(){
+  var length = prompt("How many characters would you like the password to be?")
+  while(length > 128 || length < 8 || length === null){
+    length = prompt("Password must be between 8 and 128 characters. Please enter a valid length.")
+  }
+  var includeLowercase = confirm("Do you wish to include lowercase letters? \nClick 'OK' for yes and  'Cancel' for no.")
+  var includeUppercase = confirm("Do you wish to include uppercase letters? \nClick 'OK' for yes and  'Cancel' for no.")
+  var includeNumbers = confirm("Do you wish to include numbers? \nClick 'OK' for yes and  'Cancel' for no.")
+  var includeSpecial = confirm("Do you wish to include special characters? \nClick 'OK' for yes and  'Cancel' for no.")
+  userInput = [length, includeLowercase, includeUppercase, includeNumbers, includeSpecial]
 }
 
 // Write password to the #password input
@@ -37,7 +50,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+generateBtn.addEventListener("click", promptUser);
 
 // console.log(String.fromCharCode(97))
