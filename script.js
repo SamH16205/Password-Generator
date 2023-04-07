d580a3a4e98283923d15ee3761a8e5bffb0caa85
Sam <samuelhalloum@gmail.com>
2023-04-07 16:11:50 -0500

    made a function to create a range and used it to create ranges for ascii codes.

--- script.js
+++ script.js
@@ -11,11 +11,21 @@
 
 
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
-
 var lowercase = range(97,123)
+var uppercase = range(65,91)
+var numbers = range(48, 58)
+var special = range(33,48)
 
-console.log(lowercase)
+function range(start, stop){
+  // produces a range between two values exclusive of the stop value
+  var r = []
+  for(var i=start; i<stop; i++){
+    r.push(i)
+  }
+  return r
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -30,4 +40,4 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-console.log(String.fromCharCode(97))
+// console.log(String.fromCharCode(97))
