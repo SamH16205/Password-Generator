ab9312dcea4b7e44e0372709f19b96e7e749e0dc
Sam <samuelhalloum@gmail.com>
2023-04-07 17:46:15 -0500

    made generatePassword function that makes a password from the user input

--- script.js
+++ script.js
@@ -29,16 +29,51 @@ function range(start, stop){
 }
 
 function promptUser(){
+  // Asks user for the length of password and what characters to include and updates the global variable userInput with the length as the first item in the array and a list of ascii ranges in an array as the second item. Also clears the userInput variable.
+  userInput = []
+  specChar = []
   var length = prompt("How many characters would you like the password to be?")
   while(length > 128 || length < 8 || length === null){
     length = prompt("Password must be between 8 and 128 characters. Please enter a valid length.")
   }
+  userInput.push(Number(length))
+
   var includeLowercase = confirm("Do you wish to include lowercase letters? \nClick 'OK' for yes and  'Cancel' for no.")
+  if (includeLowercase){
+    specChar.push(lowercase)
+  }
+
   var includeUppercase = confirm("Do you wish to include uppercase letters? \nClick 'OK' for yes and  'Cancel' for no.")
+  if (includeUppercase){
+    specChar.push(uppercase)
+  }
+
   var includeNumbers = confirm("Do you wish to include numbers? \nClick 'OK' for yes and  'Cancel' for no.")
+  if (includeNumbers){
+    specChar.push(numbers)
+  }
+
   var includeSpecial = confirm("Do you wish to include special characters? \nClick 'OK' for yes and  'Cancel' for no.")
-  userInput = [length, includeLowercase, includeUppercase, includeNumbers, includeSpecial]
+  if (includeSpecial){
+    specChar.push(special)
+  }
+  userInput.push(specChar)
 }
+
+function generatePassword(param){
+// takes the userInput variable as a parameter and uses it to construct the password
+var pWord = ''
+var len = param[0]
+var symbols = param[1]
+for(var i=0; i<len; i++){
+  var randSymbolRange= symbols[Math.floor(Math.random()*symbols.length)]
+  var randSymbol = randSymbolRange[Math.floor(Math.random()*randSymbolRange.length)]
+  pWord += String.fromCharCode(randSymbol)
+}
+return pWord
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
